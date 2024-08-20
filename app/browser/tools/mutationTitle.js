eb5c7a77223e6bfcbbaeb03b1b480ab725fc67f7
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2024-08-20 22:04:44 +0100

    WIP of mutation logic working with the mutation observer pattern. Not great as it flickers sometimes

--- app/browser/tools/mutationTitle.js
+++ app/browser/tools/mutationTitle.js
@@ -1,6 +1,9 @@
+let config;
+
 class MutationObserverTitle {
-    
-	init(config) {
+
+	init(newConfig) {
+		config = newConfig;
 		if (config.useMutationTitleLogic) {
 			console.debug('MutationObserverTitle enabled');
 			window.addEventListener('DOMContentLoaded', this._applyMutationToTitleLogic);
@@ -11,12 +14,18 @@ class MutationObserverTitle {
 		console.debug('Appliying MutationObserverTitle logic');
 		const observer = new window.MutationObserver(
 			() => {
-				console.debug(`title changed to ${window.document.title}`);
+				observer.disconnect();
+				console.debug(`detected title changed to ${window.document.title}`);
+				if (config.appTitle != 'Microsoft Teams') {
+					console.debug(`adding config.appTitle ${config.appTitle} to title`);
+					window.document.title = window.document.title.replace(/Microsoft Teams/i, config.appTitle);
+				}
 				const regex = /^\((\d+)\)/;
 				const match = regex.exec(window.document.title);
 				const number = match ? match[1] : 0;
 				const event = new CustomEvent('unread-count', { detail: { number: number } });
 				window.dispatchEvent(event);
+				observer.observe(window.document.querySelector('title'),{ childList: true });
 			}
 		);
 		observer.observe(window.document.querySelector('title'),{ childList: true });
